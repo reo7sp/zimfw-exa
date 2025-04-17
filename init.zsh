ee4c74a4e9cdf1319b6ef45bf9f2046e9e011fb9
Oleg Morozenkov <omorozenkov@gmail.com>
2025-04-17 19:46:18 +0300

    Update init.zsh

--- init.zsh
+++ init.zsh
@@ -12,15 +12,16 @@ export EZA_COLORS='da=1;34:gm=1;34:Su=1;34'
 
 alias ls='eza --group-directories-first'
 
+alias la='ls -a'              # Short format, all files
+
 if eza --git /dev/null &>/dev/null; then
-  alias ll='ls -l --git'      # Long format, git status
+  alias ll='ls -l -h --git'   # Long format, git status
 else
-  alias ll='ls -l'            # Long format
+  alias ll='ls -l -h'         # Long format
 fi
 
-alias l='ll -a'               # Long format, all files
 alias lr='ll -T'              # Long format, recursive as a tree
 alias lx='ll -sextension'     # Long format, sort by extension
-alias lk='ll -ssize'          # Long format, largest file size last
+alias lb='ll -ssize'          # Long format, largest file size last
 alias lt='ll -smodified'      # Long format, newest modification time last
 alias lc='ll -schanged'       # Long format, newest status change (ctime) last
